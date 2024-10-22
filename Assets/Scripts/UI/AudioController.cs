d05e71df1e37aa94c3c127df3cf43003cb42c0ec
Muhammad Fariz <muhammadfarizh31@gmail.com>
2024-10-22 13:23:11 +0700

    add button hover animation in Main Menu

--- Assets/Scripts/UI/AudioController.cs
+++ Assets/Scripts/UI/AudioController.cs
@@ -1,18 +1,26 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class AudioController : MonoBehaviour
+public class AudioController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     private Toggle _toggle;
+    private Animator _animator;
 
     [SerializeField] private Sprite _onSprite;
     [SerializeField] private Sprite _offSprite;
+    
+    private static readonly int Highlighted = Animator.StringToHash("Highlighted");
+    private static readonly int Normal = Animator.StringToHash("Normal");
+
     private void Awake()
     {
         _toggle = GetComponent<Toggle>();
+        _animator = GetComponent<Animator>();
     }
 
     public void OnValueChanged()
@@ -25,5 +33,16 @@ public class AudioController : MonoBehaviour
         {
             _toggle.image.sprite = _offSprite;
         }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _animator.SetTrigger(Highlighted);
+    }
+
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _animator.SetTrigger(Normal);
     }
 }
